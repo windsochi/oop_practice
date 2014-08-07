edb2e6236d9490836ca6020b0413fff9ab4b7270
Litos <windsochi@gmail.com>
2014-08-08 01:25:25 +0400

    git init

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,6 +1,33 @@
 class Notebook
+	attr_accessor :array
+	def add_note
+		@notebook = Array.new
+		@notebook[0] = "bloknot1"
+	end
+	def clean
+		@notebook.clear
+		puts 'Чисто: ' + (@notebook.inspect)
+	end
+	class SharedNotebook
+		def add_note
+			@shared = true
+		end
+		def count
+		end
+	end
 end
 
 class Note
+	attr_accessor :shared, :text
+  def share
+  	@shared = true
+	end
 end
 
+#вывод
+notebook = Notebook.new
+notebook.add_note
+
+note = Note.new
+note.shared = false
+note.text = 'Hello, world.'
